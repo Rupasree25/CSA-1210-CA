7310e27826f3d880fa1ebf5a437489deac86fba2
Rupasree25 <sainagarupasree@gmail.com>
2026-08-11 10:41:49 +0530

    expt 21.asm

---
+++ expt 21.asm
@@ -0,0 +1,22 @@
+MVI B, 04H
+
+LOOP1: LXI H, 2050H
+       MVI C, 04H
+
+LOOP2: MOV A, M
+       INX H
+       CMP M
+       JNC NEXT
+       
+       MOV D, M
+       MOV M, A
+       DCX H
+       MOV M, D
+       INX H
+
+NEXT:  DCR C
+       JNZ LOOP2
+       DCR B
+       JNZ LOOP1
+
+HLT
